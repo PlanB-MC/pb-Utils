905f65ff2c98092d28d19cd7b1fa5f1bf6acc8cd
Valicious <gert.pe.agg@gmail.com>
2018-07-24 12:50:33 +0200

    blocklist update

--- src/planB/mc/val/barrier/onGlassDetect.java
+++ src/planB/mc/val/barrier/onGlassDetect.java
@@ -10,19 +10,19 @@ public class onGlassDetect implements Listener {
     @EventHandler
     public void onGlassPlaced(BlockPlaceEvent event) {
         if (event.getBlockPlaced().getType().toString().contains("GLASS")) {
-            int amount = Main.pbConfigFile.getInt("amount", "pbExtraOutput", "glassPlaced") + 1;
-            Main.pbConfigFile.set("amount", amount, "pbExtraOutput", "glassPlaced");
+            int amount = Main.pbConfigFile.getInt("amount", "pbBarrier") + 1;
+            Main.pbConfigFile.set("amount", amount, "pbBarrier");
             String pUUID = event.getPlayer().getUniqueId().toString();
-            MemorySection player = (MemorySection) Main.pbConfigFile.get(pUUID, "pbExtraOutput", "glassPlaced", "players");
+            MemorySection player = (MemorySection) Main.pbConfigFile.get(pUUID, "pbBarrier", "players");
             ActivePlayer curPLayer = null;
             if (player != null)
                 curPLayer = new ActivePlayer(player.getInt("score"), player.getString("name"), player.getString("uuid"));
             else
                 curPLayer = new ActivePlayer(0, event.getPlayer().getName(), pUUID);
 
-            Main.pbConfigFile.set("name", curPLayer.name, "pbExtraOutput", "glassPlaced", "players", curPLayer.UUID);
-            Main.pbConfigFile.set("uuid", curPLayer.UUID, "pbExtraOutput", "glassPlaced", "players", curPLayer.UUID);
-            Main.pbConfigFile.set("score", curPLayer.score, "pbExtraOutput", "glassPlaced", "players", curPLayer.UUID);
+            Main.pbConfigFile.set("name", curPLayer.name, "pbBarrier", "players", curPLayer.UUID);
+            Main.pbConfigFile.set("uuid", curPLayer.UUID, "pbBarrier", "players", curPLayer.UUID);
+            Main.pbConfigFile.set("score", curPLayer.score, "pbBarrier", "players", curPLayer.UUID);
         }
     }
 
@@ -32,7 +32,7 @@ public class onGlassDetect implements Listener {
         String UUID;
 
         public ActivePlayer(int score, String name, String UUID) {
-            System.out.println(score + " " + name + " " + UUID);
+            //System.out.println(score + " " + name + " " + UUID);
             this.score = score + 1;
             this.name = name;
             this.UUID = UUID;
--- src/planB/mc/val/pbUtils/pbCrafting.java
+++ src/planB/mc/val/pbUtils/pbCrafting.java
@@ -3,16 +3,120 @@ package planB.mc.val.pbUtils;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
 import planB.mc.val.Main;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class pbCrafting {
+    private int count = 0;
+    private Main plugin;
 
     public pbCrafting(Main plugin) {
-        NamespacedKey key = new NamespacedKey(plugin, plugin.getDescription().getName());
+        this.plugin = plugin;
+        //initialize Stairs
+        List<Recipe> restore = new ArrayList<>();
+        HashMap<Material, ItemStack> stairs = new HashMap<>();
+        stairs.put(Material.ACACIA_PLANKS, new ItemStack(Material.ACACIA_STAIRS, 8));
+        stairs.put(Material.BIRCH_PLANKS, new ItemStack(Material.BIRCH_STAIRS, 8));
+        stairs.put(Material.BRICK, new ItemStack(Material.BRICK_STAIRS, 8));
+        stairs.put(Material.COBBLESTONE, new ItemStack(Material.COBBLESTONE_STAIRS, 8));
+        stairs.put(Material.DARK_OAK_PLANKS, new ItemStack(Material.DARK_OAK_STAIRS, 8));
+        stairs.put(Material.DARK_PRISMARINE, new ItemStack(Material.DARK_PRISMARINE_STAIRS, 8));
+        stairs.put(Material.JUNGLE_PLANKS, new ItemStack(Material.JUNGLE_STAIRS, 8));
+        stairs.put(Material.NETHER_BRICK, new ItemStack(Material.NETHER_BRICK_STAIRS, 8));
+        stairs.put(Material.OAK_PLANKS, new ItemStack(Material.OAK_STAIRS, 8));
+        stairs.put(Material.PRISMARINE_BRICKS, new ItemStack(Material.PRISMARINE_BRICK_STAIRS, 8));
+        stairs.put(Material.PURPUR_BLOCK, new ItemStack(Material.PURPUR_STAIRS, 8));
+        stairs.put(Material.RED_SANDSTONE, new ItemStack(Material.RED_SANDSTONE_STAIRS, 8));
+        stairs.put(Material.QUARTZ, new ItemStack(Material.QUARTZ_STAIRS, 8));
+        stairs.put(Material.SANDSTONE, new ItemStack(Material.SANDSTONE_STAIRS, 8));
+        stairs.put(Material.SPRUCE_PLANKS, new ItemStack(Material.SPRUCE_STAIRS, 8));
+        stairs.put(Material.STONE_BRICKS, new ItemStack(Material.STONE_BRICK_STAIRS, 8));
+        stairs.put(Material.PRISMARINE, new ItemStack(Material.PRISMARINE_STAIRS, 8));
+
+        //initialize Wool
+        HashMap<Material, ItemStack> wool = new HashMap<>();
+        stairs.put(Material.INK_SAC, new ItemStack(Material.BLACK_WOOL, 8));
+        stairs.put(Material.LAPIS_LAZULI, new ItemStack(Material.BLUE_WOOL, 8));
+        stairs.put(Material.COCOA_BEANS, new ItemStack(Material.BROWN_WOOL, 8));
+        stairs.put(Material.CYAN_DYE, new ItemStack(Material.CYAN_WOOL, 8));
+        stairs.put(Material.GRAY_DYE, new ItemStack(Material.GRAY_WOOL, 8));
+        stairs.put(Material.CACTUS_GREEN, new ItemStack(Material.GREEN_WOOL, 8));
+        stairs.put(Material.LIGHT_BLUE_DYE, new ItemStack(Material.LIGHT_BLUE_WOOL, 8));
+        stairs.put(Material.LIGHT_GRAY_DYE, new ItemStack(Material.LIGHT_GRAY_WOOL, 8));
+        stairs.put(Material.LIME_DYE, new ItemStack(Material.LIME_WOOL, 8));
+        stairs.put(Material.MAGENTA_DYE, new ItemStack(Material.MAGENTA_WOOL, 8));
+        stairs.put(Material.ORANGE_DYE, new ItemStack(Material.ORANGE_WOOL, 8));
+        stairs.put(Material.PINK_DYE, new ItemStack(Material.PINK_WOOL, 8));
+        stairs.put(Material.PURPLE_DYE, new ItemStack(Material.PURPLE_WOOL, 8));
+        stairs.put(Material.ROSE_RED, new ItemStack(Material.RED_WOOL, 8));
+        stairs.put(Material.DANDELION_YELLOW, new ItemStack(Material.YELLOW_WOOL, 8));
+
+        //remove all recipes
+        plugin.getServer().recipeIterator().forEachRemaining(cur -> {
+            ItemStack result = cur.getResult();
+            result.setAmount(8);
+
+            boolean found = false;
+            for (ItemStack curItem : stairs.values()) {
+                if (result.isSimilar(curItem)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                /*for (ItemStack curItem : wool.values()) {
+                    if (result.getType().equals(curItem.getType())) {
+                        found = true;
+                        break;
+                    }
+                }*/
+                if (!found) {
+                    restore.add(cur);
+                    return;
+                }
+            }
+        });
+        plugin.getServer().clearRecipes();
+        //set new recipes
+
+        //  Crafting recipe for Stair
+        stairs.forEach((curMat, curStair) -> {
+            ShapedRecipe newRecipeR = new ShapedRecipe(getKey(), curStair);
+            ShapedRecipe newRecipeL = new ShapedRecipe(getKey(), curStair);
+            newRecipeR.shape("**b", "*bb", "bbb");
+            newRecipeR.setIngredient('b', curMat);
+            newRecipeL.shape("b**", "bb*", "bbb");
+            newRecipeL.setIngredient('b', curMat);
+            plugin.getServer().addRecipe(newRecipeR);
+            plugin.getServer().addRecipe(newRecipeL);
+        });
+        //  Crafting recipe for Wool
+       /* wool.forEach((curMat, curWool) -> {
+            ShapedRecipe newRecipe = new ShapedRecipe(getKey(), curWool);
+            newRecipe.shape("bbb", "b*b", "bbb");
+            newRecipe.setIngredient('*', curMat);
+            newRecipe.setIngredient('b', Material.WHITE_WOOL);
+            plugin.getServer().addRecipe(newRecipe);
+        });*/
+
+        //restore recipes
+        for (Recipe cur : restore) {
+            try {
+                plugin.getServer().addRecipe(cur);
+            } catch (IllegalStateException e) {
+            }
+        }
+
+        //New recipes
         //Crafting recipe for Barrier Block
         ItemStack barrierStack = new ItemStack(Material.BARRIER, 32);
-        ShapedRecipe barrierR = new ShapedRecipe(key, barrierStack);
+        ShapedRecipe barrierR = new ShapedRecipe(getKey(), barrierStack);
         barrierR.shape("GSG", "GBG", "GPG");
         barrierR.setIngredient('G', Material.GLASS);
         barrierR.setIngredient('B', Material.BUCKET);
@@ -20,176 +124,41 @@ public class pbCrafting {
         barrierR.setIngredient('P', Material.BLAZE_POWDER);
         plugin.getServer().addRecipe(barrierR);
 
+        //Crafting recipes for slabs
+        HashMap<Material, ItemStack> slabs = new HashMap<>();
+        slabs.put(Material.ACACIA_SLAB, new ItemStack(Material.ACACIA_PLANKS));
+        slabs.put(Material.BIRCH_SLAB, new ItemStack(Material.BIRCH_PLANKS));
+        slabs.put(Material.BRICK_SLAB, new ItemStack(Material.BRICKS));
+        slabs.put(Material.SANDSTONE_SLAB, new ItemStack(Material.SANDSTONE));
+        slabs.put(Material.SPRUCE_SLAB, new ItemStack(Material.SPRUCE_PLANKS));
+        slabs.put(Material.COBBLESTONE_SLAB, new ItemStack(Material.COBBLESTONE));
+        slabs.put(Material.DARK_OAK_SLAB, new ItemStack(Material.DARK_OAK_PLANKS));
+        slabs.put(Material.DARK_PRISMARINE_SLAB, new ItemStack(Material.DARK_PRISMARINE));
+        slabs.put(Material.JUNGLE_SLAB, new ItemStack(Material.JUNGLE_PLANKS));
+        slabs.put(Material.NETHER_BRICK_SLAB, new ItemStack(Material.NETHER_BRICK));
+        slabs.put(Material.OAK_SLAB, new ItemStack(Material.OAK_PLANKS));
+        slabs.put(Material.STONE_SLAB, new ItemStack(Material.STONE));
+        slabs.put(Material.STONE_BRICK_SLAB, new ItemStack(Material.STONE_BRICKS));
+        slabs.put(Material.PURPUR_SLAB, new ItemStack(Material.PURPUR_BLOCK));
+        slabs.put(Material.RED_SANDSTONE_SLAB, new ItemStack(Material.RED_SANDSTONE));
+        slabs.put(Material.QUARTZ_SLAB, new ItemStack(Material.QUARTZ_BLOCK));
+        slabs.put(Material.PRISMARINE_SLAB, new ItemStack(Material.PRISMARINE));
+        slabs.put(Material.PRISMARINE_BRICK_SLAB, new ItemStack(Material.PRISMARINE_BRICKS));
 
-   /*     //Stairs
-        List<Recipe> restore = new ArrayList<>();
-        List<ItemStack> stairs = new ArrayList<>();
-        ItemStack ACACIA_STAIRSStack = new ItemStack(Material.ACACIA_STAIRS, 8);
-        ItemStack BIRCH_STAIRSStack = new ItemStack(Material.BIRCH_STAIRS, 8);
-        ItemStack BRICK_STAIRSStack = new ItemStack(Material.BRICK_STAIRS, 8);
-        ItemStack COBBLESTONE_STAIRSStack = new ItemStack(Material.COBBLESTONE_STAIRS, 8);
-        ItemStack DARK_OAK_STAIRSStack = new ItemStack(Material.DARK_OAK_STAIRS, 8);
-        ItemStack DARK_PRISMARINE_STAIRSStack = new ItemStack(Material.DARK_PRISMARINE_STAIRS, 8);
-        ItemStack JUNGLE_STAIRSStack = new ItemStack(Material.JUNGLE_STAIRS, 8);
-        ItemStack NETHER_BRICK_STAIRSStack = new ItemStack(Material.NETHER_BRICK_STAIRS, 8);
-        ItemStack OAK_STAIRSStack = new ItemStack(Material.OAK_STAIRS, 8);
-        ItemStack PRISMARINE_BRICK_STAIRSStack = new ItemStack(Material.PRISMARINE_BRICK_STAIRS, 8);
-        ItemStack PURPUR_STAIRSStack = new ItemStack(Material.PURPUR_STAIRS, 8);
-        ItemStack RED_SANDSTONE_STAIRSStack = new ItemStack(Material.RED_SANDSTONE_STAIRS, 8);
-        ItemStack QUARTZ_STAIRSStack = new ItemStack(Material.QUARTZ_STAIRS, 8);
-        ItemStack SANDSTONE_STAIRSStack = new ItemStack(Material.SANDSTONE_STAIRS, 8);
-        ItemStack SPRUCE_STAIRSStack = new ItemStack(Material.SPRUCE_STAIRS, 8);
-        ItemStack STONE_BRICK_STAIRSStack = new ItemStack(Material.STONE_BRICK_STAIRS, 8);
-        ItemStack PRISMARINE_STAIRSStack = new ItemStack(Material.PRISMARINE_STAIRS, 8);
-        stairs.addAll(Arrays.asList(
-                ACACIA_STAIRSStack,
-                BIRCH_STAIRSStack,
-                BRICK_STAIRSStack,
-                COBBLESTONE_STAIRSStack,
-                DARK_OAK_STAIRSStack,
-                DARK_PRISMARINE_STAIRSStack,
-                JUNGLE_STAIRSStack,
-                NETHER_BRICK_STAIRSStack,
-                OAK_STAIRSStack,
-                PRISMARINE_BRICK_STAIRSStack,
-                PURPUR_STAIRSStack,
-                RED_SANDSTONE_STAIRSStack,
-                QUARTZ_STAIRSStack,
-                SANDSTONE_STAIRSStack,
-                SPRUCE_STAIRSStack,
-                STONE_BRICK_STAIRSStack,
-                PRISMARINE_STAIRSStack
-        ));
-
-
-        plugin.getServer().recipeIterator().forEachRemaining(cur -> {
-            ItemStack result = cur.getResult();
-            if (!result.isSimilar(acaciaStairStack)) {
-                restore.add(cur);
-            }
+        slabs.forEach((curMat, curBlock) -> {
+            ShapelessRecipe newOne = new ShapelessRecipe(getKey(), curBlock);
+            newOne.addIngredient(2, curMat);
+            plugin.getServer().addRecipe(newOne);
         });
 
-        plugin.getServer().clearRecipes();
 
-
-        //Crafting recipe for Stair
-        //ACACIA_STAIRS
-        //ItemStack acaciaStairStack = new ItemStack(Material.ACACIA_STAIRS);
-    /*    ShapedRecipe acaciaStairR = new ShapedRecipe(key, acaciaStairStack);
-        acaciaStairR.shape("**b", "*bb", "bbb");
-        // acaciaStairR.setIngredient('*', Material.AIR);
-        acaciaStairR.setIngredient('b', Material.ACACIA_WOOD);
-        System.out.println("test");
-        plugin.getServer().addRecipe(acaciaStairR);
-
-        restore.forEach(cur -> {
-            try {
-                plugin.getServer().addRecipe(cur);
-            } catch (IllegalStateException e) {
-                //ignore
-            }
-        });
-
-  /*      //BIRCH_STAIRS
-        ItemStack Stack = new ItemStack(Material.BIRCH_STAIRS);
-        ShapedRecipe R = new ShapedRecipe(key, Stack);
-        R.shape("**b", "*bb", "bbb");
-        R.setIngredient('b', Material.);
-        plugin.getServer().addRecipe(R);
-        //BRICK_STAIRS
-        ItemStack Stack = new ItemStack(Material.BRICK_STAIRS);
-        ShapedRecipe R = new ShapedRecipe(key, Stack);
-        R.shape("**b", "*bb", "bbb");
-        R.setIngredient('b', Material.);
-        plugin.getServer().addRecipe(R);
-        //COBBLESTONE_STAIRS
-        ItemStack Stack = new ItemStack(Material.COBBLESTONE_STAIRS);
-        ShapedRecipe R = new ShapedRecipe(key, Stack);
-        R.shape("**b", "*bb", "bbb");
-        R.setIngredient('b', Material.);
-        plugin.getServer().addRecipe(R);
-        //DARK_OAK_STAIRS
-        ItemStack Stack = new ItemStack(Material.DARK_OAK_STAIRS);
-        ShapedRecipe R = new ShapedRecipe(key, Stack);
-        R.shape("**b", "*bb", "bbb");
-        R.setIngredient('b', Material.);
-        plugin.getServer().addRecipe(R);
-        //DARK_PRISMARINE_STAIRS
-        ItemStack Stack = new ItemStack(Material.DARK_PRISMARINE_STAIRS);
-        ShapedRecipe R = new ShapedRecipe(key, Stack);
-        R.shape("**b", "*bb", "bbb");
-        R.setIngredient('b', Material.);
-        plugin.getServer().addRecipe(R);
-        //JUNGLE_STAIRS
-        ItemStack Stack = new ItemStack(Material.JUNGLE_STAIRS);
-        ShapedRecipe R = new ShapedRecipe(key, Stack);
-        R.shape("**b", "*bb", "bbb");
-        R.setIngredient('b', Material.);
-        plugin.getServer().addRecipe(R);
-        //NETHER_BRICK_STAIRS
-        ItemStack Stack = new ItemStack(Material.NETHER_BRICK_STAIRS);
-        ShapedRecipe R = new ShapedRecipe(key, Stack);
-        R.shape("**b", "*bb", "bbb");
-        R.setIngredient('b', Material.);
-        plugin.getServer().addRecipe(R);
-        //OAK_STAIRS
-        ItemStack Stack = new ItemStack(Material.OAK_STAIRS);
-        ShapedRecipe R = new ShapedRecipe(key, Stack);
-        R.shape("**b", "*bb", "bbb");
-        R.setIngredient('b', Material.);
-        plugin.getServer().addRecipe(R);
-        //PRISMARINE_BRICK_STAIRS
-        ItemStack Stack = new ItemStack(Material.PRISMARINE_BRICK_STAIRS);
-        ShapedRecipe R = new ShapedRecipe(key, Stack);
-        R.shape("**b", "*bb", "bbb");
-        R.setIngredient('b', Material.);
-        plugin.getServer().addRecipe(R);
-        //PRISMARINE_STAIRS
-        ItemStack Stack = new ItemStack(Material.PRISMARINE_STAIRS);
-        ShapedRecipe R = new ShapedRecipe(key, Stack);
-        R.shape("**b", "*bb", "bbb");
-        R.setIngredient('b', Material.);
-        plugin.getServer().addRecipe(R);
-        //PURPUR_STAIRS
-        ItemStack Stack = new ItemStack(Material.PURPUR_STAIRS);
-        ShapedRecipe R = new ShapedRecipe(key, Stack);
-        R.shape("**b", "*bb", "bbb");
-        R.setIngredient('b', Material.);
-        plugin.getServer().addRecipe(R);
-        //QUARTZ_STAIRS
-        ItemStack Stack = new ItemStack(Material.QUARTZ_STAIRS);
-        ShapedRecipe R = new ShapedRecipe(key, Stack);
-        R.shape("**b", "*bb", "bbb");
-        R.setIngredient('b', Material.);
-        plugin.getServer().addRecipe(R);
-        //RED_SANDSTONE_STAIRS
-        ItemStack Stack = new ItemStack(Material.RED_SANDSTONE_STAIRS);
-        ShapedRecipe R = new ShapedRecipe(key, Stack);
-        R.shape("**b", "*bb", "bbb");
-        R.setIngredient('b', Material.);
-        plugin.getServer().addRecipe(R);
-        //SANDSTONE_STAIRS
-        ItemStack Stack = new ItemStack(Material.SANDSTONE_STAIRS);
-        ShapedRecipe R = new ShapedRecipe(key, Stack);
-        R.shape("**b", "*bb", "bbb");
-        R.setIngredient('b', Material.);
-        plugin.getServer().addRecipe(R);
-        //SPRUCE_STAIRS
-        ItemStack Stack = new ItemStack(Material.SPRUCE_STAIRS);
-        ShapedRecipe R = new ShapedRecipe(key, Stack);
-        R.shape("**b", "*bb", "bbb");
-        R.setIngredient('b', Material.);
-        plugin.getServer().addRecipe(R);
-        //STONE_BRICK_STAIRS
-        ItemStack Stack = new ItemStack(Material.STONE_BRICK_STAIRS);
-        ShapedRecipe R = new ShapedRecipe(key, Stack);
-        R.shape("**b", "*bb", "bbb");
-        R.setIngredient('b', Material.);
-        plugin.getServer().addRecipe(R);*/
-
-
-        pbUtils.log("[pbCrafting]", "Added a total of 3 Recipe(s)");
+        pbUtils.log("[pbCrafting]", "Added a total of " + count + " Recipe(s)");
     }
 
+    private NamespacedKey getKey() {
+        NamespacedKey key = new NamespacedKey(plugin, "key_val_" + count);
+        count++;
+        return key;
+    }
 
 }
